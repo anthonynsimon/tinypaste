4a520bbd17fa5b889d0bbd756d8c8cf8d094066e
Anthony Najjar <anthonynsimon@gmail.com>
2017-04-12 11:19:49 +0200

    Implement get and create paste routes

--- pastes_api/pastes_api/routes.py
+++ pastes_api/pastes_api/routes.py
@@ -4,19 +4,23 @@ from flask import request
 
 from pastes_api import app, db
 from pastes_api.model import Paste, paste_schema
-from pastes_api.render import json_response
+from pastes_api.render import json_response, json_error
 
 
 @app.route("/pastes/<id>", methods=['GET'])
 def index(id):
-    return id
+    paste = db.session.query(Paste).get(id)
+    result = paste_schema.dump(paste).data
+    app.logger.debug(result)
+    if result is None or not result:
+        return json_error(404, "no paste with such id found")
+    return json_response(200, result)
 
 
 @app.route("/pastes", methods=['POST'])
 def create_paste():
     paste = paste_schema.load(json.loads(request.data)).data
-    # result = paste_schema.dump(paste).data
-    return json_response(201, paste)
     db.session.add(paste)
     db.session.commit()
-    return paste
+    result = paste_schema.dump(paste).data
+    return json_response(201, result)
