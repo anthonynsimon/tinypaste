08935d3d124f8a667b4b567278bd5ec31285421f
Anthony Najjar <anthonynsimon@gmail.com>
2017-04-12 11:19:40 +0200

    Update paste model

--- pastes_api/pastes_api/model.py
+++ pastes_api/pastes_api/model.py
@@ -1,4 +1,4 @@
-import time
+import datetime
 
 from marshmallow import post_load, fields
 
@@ -7,27 +7,20 @@ from pastes_api import db, marshall
 
 class Paste(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.Integer)
-    content = db.Column(db.String(2048))
+    created_at = db.Column(db.String(32), nullable=True)
+    content = db.Column(db.String(2048), nullable=False)
 
     def __init__(self, content, created_at=None):
-        self.created_at = created_at if created_at is not None else int(
-            time.time())
         self.content = content
+        if created_at is not None:
+            self.created_at = created_at
+        else:
+            self.created_at = datetime.datetime.now().isoformat()
 
 
 class PasteSchema(marshall.ModelSchema):
     class Meta:
         model = Paste
-
-    id = fields.Int()
-    content = fields.String()
-    created_at = fields.DateTime()
-
-    @post_load
-    def make_paste(self, data):
-        print(data)
-        return Paste(**data)
 
 
 paste_schema = PasteSchema()
