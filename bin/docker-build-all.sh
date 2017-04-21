029d9ae635e3aeac9329798a177c3f2c33522b49
Anthony Najjar <anthonynsimon@gmail.com>
2017-04-21 10:54:24 +0200

    Fix docker build logstash tag

--- bin/docker-build-all.sh
+++ bin/docker-build-all.sh
@@ -2,6 +2,6 @@
 
 cd $(dirname $0)
 
-docker build --rm -t logstash ../logstash
+docker build --rm -t tinypaste/logstash ../logstash
 docker build --rm -t pastes-api ../pastes-api
 docker build --rm -t shorten-id-api ../shorten-id-api
